8a3bc0f6cadf27a8e29c914a379fd6ab017af53a
DevAch33 <achaibane.ayoub@gmail.com>
2023-07-06 14:07:37 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,5 +9,26 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int btu;
+	int len , base;
+
+	if(!b)
+		return(0);
+	btu = 0;
+
+	for (len = 0; b[len] != '\0' ; len++)
+		;
+	for (len--, base = 1; len >= 0; len--, base *= 2)
+	{
+		if (b[len] != '0' && b[len] != '1')
+		{
+			return (0);
+		}
+		if (b[len] & 1)
+		{
+			btu += base;
+		}
+	}
+	return (btu);
 
 }

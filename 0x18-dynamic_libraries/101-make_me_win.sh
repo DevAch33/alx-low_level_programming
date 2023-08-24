1a766b815bae958f61eeb10db04253ad14112002
DevAch33 <124171054+DevAch33@users.noreply.github.com>
2023-08-24 23:47:36 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/DevAch33/alx-low_level_programming/edit/master/0x18-dynamic_libraries/programtest.so
-export LD_PRELOAD=/tmp/programtest.so
+wget -P /tmp https://github.com/DevAch33/alx-low_level_programming/edit/master/0x18-dynamic_libraries/cheatme.so
+export LD_PRELOAD=/tmp/cheatme.so
